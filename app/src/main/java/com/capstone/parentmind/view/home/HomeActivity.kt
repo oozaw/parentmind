69d6918a90c3df566bcd8ce8a1b2bc4f51988d6b
oozaw <79121990+oozaw@users.noreply.github.com>
2022-11-10 10:34:48 +0700

    add package structure and dependencies

--- app/src/main/java/com/capstone/parentmind/view/home/HomeActivity.kt
+++ app/src/main/java/com/capstone/parentmind/view/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.capstone.parentmind
+package com.capstone.parentmind.view.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.capstone.parentmind.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
-      setContentView(R.layout.activity_main)
+      setContentView(R.layout.activity_home)
    }
 }
