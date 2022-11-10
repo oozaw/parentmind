9b54661fb79b5cdcf552a178b36945a7e23bb12d
oozaw <79121990+oozaw@users.noreply.github.com>
2022-11-10 10:46:03 +0700

    initialize dagger-hilt

---
+++ app/src/main/java/com/capstone/parentmind/ParentMindApplication.kt
@@ -0,0 +1,7 @@
+package com.capstone.parentmind
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ParentMindApplication: Application()
--- app/src/main/java/com/capstone/parentmind/view/home/HomeActivity.kt
+++ app/src/main/java/com/capstone/parentmind/view/home/HomeActivity.kt
@@ -3,7 +3,9 @@ package com.capstone.parentmind.view.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.parentmind.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
