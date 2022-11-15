01cb33a7a2cac2b080ff5d661f096696eb038e15
oozaw <79121990+oozaw@users.noreply.github.com>
2022-11-16 00:13:42 +0700

    add firebase google auth backend

--- app/src/main/java/com/capstone/parentmind/view/home/HomeActivity.kt
+++ app/src/main/java/com/capstone/parentmind/view/home/HomeActivity.kt
@@ -1,14 +1,46 @@
 package com.capstone.parentmind.view.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstone.parentmind.R
+import com.capstone.parentmind.databinding.ActivityHomeBinding
+import com.capstone.parentmind.view.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
+   private var _binding: ActivityHomeBinding? = null
+   private val binding get()= _binding!!
+
+   private lateinit var auth: FirebaseAuth
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
-      setContentView(R.layout.activity_home)
+      _binding = ActivityHomeBinding.inflate(layoutInflater)
+      setContentView(binding.root)
+
+      auth = Firebase.auth
+
+      setupAction()
+   }
+
+   override fun onDestroy() {
+      super.onDestroy()
+      _binding = null
+   }
+
+   private fun setupAction() {
+      binding.btnToLoginPage.setOnClickListener {
+         val loginIntent = Intent(this, LoginActivity::class.java)
+         startActivity(loginIntent)
+      }
+
+      binding.btnLogout.setOnClickListener {
+         auth.signOut()
+      }
    }
 }
---
+++ app/src/main/java/com/capstone/parentmind/view/login/LoginActivity.kt
@@ -0,0 +1,111 @@
+package com.capstone.parentmind.view.login
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import com.capstone.parentmind.R
+import com.capstone.parentmind.databinding.ActivityLoginBinding
+import com.capstone.parentmind.view.home.HomeActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+   private var _binding : ActivityLoginBinding? = null
+   private val binding get()= _binding!!
+
+   private lateinit var googleSignInClient: GoogleSignInClient
+   private lateinit var auth: FirebaseAuth
+
+   private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+      if (result.resultCode == Activity.RESULT_OK) {
+         val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+         try {
+            val account = task.getResult(ApiException::class.java)
+            Log.d(TAG, getString(R.string.firebase_auth_google, account.id))
+            account.idToken?.let { firebaseAuthWithGoogle(it) }
+         } catch (e: ApiException) {
+            Log.w(TAG, "Google sign in failed", e)
+         }
+      }
+   }
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      _binding = ActivityLoginBinding.inflate(layoutInflater)
+      setContentView(binding.root)
+
+      // Configure Google Sign In
+      val gso = GoogleSignInOptions
+         .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+         .requestIdToken(getString(R.string.default_web_client_id))
+         .requestEmail()
+         .build()
+
+      googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+      // Initialize Firebase Auth
+      auth = Firebase.auth
+
+      setupAction()
+   }
+
+   override fun onStart() {
+      super.onStart()
+      // Check if user is signed in (non-null) and update UI accordingly.
+      val currentUser = auth.currentUser
+      updateUI(currentUser)
+   }
+
+   override fun onDestroy() {
+      super.onDestroy()
+      _binding = null
+   }
+
+   private fun setupAction() {
+      binding.btnLogin.setOnClickListener {
+         googleLogin()
+      }
+   }
+
+   private fun googleLogin() {
+      val loginIntent = googleSignInClient.signInIntent
+      resultLauncher.launch(loginIntent)
+   }
+
+   private fun firebaseAuthWithGoogle(idToken: String) {
+      val credential = GoogleAuthProvider.getCredential(idToken, null)
+      auth.signInWithCredential(credential)
+         .addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+               Log.d(TAG, "signInWithCredential: success")
+               val user = auth.currentUser
+               updateUI(user)
+            } else {
+               Log.w(TAG, "signInWithCredential: failure ", task.exception )
+               updateUI(null)
+            }
+         }
+   }
+
+   private fun updateUI(currentUser: FirebaseUser?) {
+      if (currentUser != null) {
+         val homeIntent = Intent(this, HomeActivity::class.java)
+         startActivity(homeIntent)
+         finish()
+      }
+   }
+
+   companion object {
+      private const val TAG = "LoginActivity"
+   }
+}
