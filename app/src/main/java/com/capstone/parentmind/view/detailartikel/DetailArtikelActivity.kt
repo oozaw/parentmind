6b5aad8f9a1c82fa2153f15fca7e30fda394aa03
Wahyu Purnomo Ady <79121990+oozaw@users.noreply.github.com>
2022-11-28 18:03:32 +0700

    Commit Layout (#4)
    * Commit Layout
    
    * resolve conflict
    
    Co-authored-by: Afifafa <afifafafa127@gmail.com>

---
+++ app/src/main/java/com/capstone/parentmind/view/detailartikel/DetailArtikelActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.parentmind.view.detailartikel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.parentmind.R
+
+class DetailArtikelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_artikel)
+    }
+}
