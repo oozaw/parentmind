375274a4d3202878f715aa41739a2459bf0bc3c0
Wahyu Purnomo Ady <79121990+oozaw@users.noreply.github.com>
2022-11-28 19:38:21 +0700

    test commit (#5)
    * test commit
    
    * resolve conflict
    
    * resolve conflict #2
    
    Co-authored-by: Ari Lathifatul Chusna <ari.lathifatul.chusna-2020@vokasi.unair.ac.id>

---
+++ app/src/main/java/com/capstone/parentmind/view/forum/ForumActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.parentmind.view.forum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.parentmind.R
+
+class ForumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forum)
+    }
+}
