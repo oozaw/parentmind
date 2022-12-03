ae7c73fb4e815b2524543dad731df396f79ee2d1
Ari Lathifatul Chusna <ari.lathifatul.chusna-2020@vokasi.unair.ac.id>
2022-12-03 09:09:54 +0700

    layout forgot dkk

---
+++ app/src/main/java/com/capstone/parentmind/view/login/forgotpassword/ForgotPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.parentmind.view.login.forgotpassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.parentmind.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
